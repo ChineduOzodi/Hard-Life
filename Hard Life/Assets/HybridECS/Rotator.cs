3c00e35e331b97c7c2ac6162974e51b82d07295a
Chinedu Ozodi <chinedu.ozodi@gmail.com>
2018-09-11 17:14:47 -0400

    Added Entities and created HybridECS

---
+++ Hard Life/Assets/HybridECS/Rotator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    public float speed;
+}
+
+class RotatorSystem : ComponentSystem
+{
+    struct Components
+    {
+        public Rotator rotator;
+        public Transform transform;
+    }
+
+    //Behavior
+    protected override void OnUpdate()
+    {
+        foreach(var entity in GetEntities<Components>())
+        {
+            entity.transform.Rotate(0, entity.rotator.speed, 0);
+        } 
+    }
+}
